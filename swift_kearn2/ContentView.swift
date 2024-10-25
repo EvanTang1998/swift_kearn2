60adc263dca15b31a4e9ac3f472df0146d6c25fd
v_tangshen <v_tangshen@baidu.com>
2024-10-25 16:05:05 +0800

    some test

--- swift_kearn2/ContentView.swift
+++ swift_kearn2/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Hello, Evan!")
-            Text("some test")
         }
         .padding()
     }
