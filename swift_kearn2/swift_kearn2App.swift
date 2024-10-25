7c42e1c7e643b5e6753718a8fb8c6060812de8ca
v_tangshen <v_tangshen@baidu.com>
2024-10-25 15:18:58 +0800

    Initial Commit

---
+++ swift_kearn2/swift_kearn2App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_kearn2App.swift
+//  swift_kearn2
+//
+//  Created by tangshen on 2024/10/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_kearn2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
