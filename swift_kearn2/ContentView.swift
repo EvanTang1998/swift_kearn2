0b95b2652d26ebdad9eba72faf23ffe6aa0b5c50
v_tangshen <v_tangshen@baidu.com>
2024-10-25 15:32:02 +0800

    first commit

--- swift_kearn2/ContentView.swift
+++ swift_kearn2/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Hello, Evan!")
+            Text("some test")
         }
         .padding()
     }
